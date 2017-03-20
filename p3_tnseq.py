4fc4f732d86a9fbc1b87a03ccefed20e7fd6dec7
Andrew Warren <anwarren@vbi.vt.edu>
2017-03-20 18:26:08 -0400

    further modifications for running the tpp

--- p3_tnseq.py
+++ p3_tnseq.py
@@ -1,76 +1,86 @@
-
 #!/usr/bin/env 
 import os, sys
 import argparse
 import requests
+import json
 
 
 def get_genome(parameters):
-    genome_url= "data_url/genome_sequence/?eq(genome_id,gid)&limit(25000)".replace("data_url",parameters["data_url"]).replace("gid",parameters["gid"])
-    print genome_url
-    headers = {"accept":"application/sralign+dna+fasta"}
-    #print "switch THE HEADER BACK!"
-    #headers = {'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'}
-    req = requests.Request('GET', genome_url, headers=headers)
-    prepared = req.prepare()
-    #pretty_print_POST(prepared)
-    s = requests.Session()
-    response=s.send(prepared)
-    handle = open(os.path.join(parameters["output_dir"],parameters["gid"]+".fna"), 'w')
-    if not response.ok:
-        sys.stderr.write("API not responding. Please try again later.\n")
-        sys.exit(2)
-    else:
-        for block in response.iter_content(1024):
-            handle.write(block)
+    target_file = os.path.join(parameters["output_path"],parameters["gid"]+".fna")
+    if not os.path.exists(target_file):
+        genome_url= "data_url/genome_sequence/?eq(genome_id,gid)&limit(25000)".replace("data_url",parameters["data_url"]).replace("gid",parameters["gid"])
+        print genome_url
+        headers = {"accept":"application/sralign+dna+fasta"}
+        #print "switch THE HEADER BACK!"
+        #headers = {'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'}
+        req = requests.Request('GET', genome_url, headers=headers)
+        prepared = req.prepare()
+        #pretty_print_POST(prepared)
+        s = requests.Session()
+        response=s.send(prepared)
+        handle = open(target_file, 'w')
+        if not response.ok:
+            sys.stderr.write("API not responding. Please try again later.\n")
+            sys.exit(2)
+        else:
+            for block in response.iter_content(1024):
+                handle.write(block)
+    return target_file
 
 def get_annotation(parameters):
-    annotation_url= "data_url/genome_feature/?and(eq(genome_id,gid),eq(annotation,PATRIC),or(eq(feature_type,CDS),eq(feature_type,tRNA),eq(feature_type,rRNA)))&limit(25000)".replace("data_url",parameters["data_url"]).replace("gid",parameters["gid"])
-    print annotation_url
-    headers = {"accept":"application/cufflinks+gff"}
-    #print "switch THE HEADER BACK!"
-    #headers = {'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'}
-    req = requests.Request('GET', annotation_url, headers=headers)
-    prepared = req.prepare()
-    #pretty_print_POST(prepared)
-    s = requests.Session()
-    response=s.send(prepared)
-    handle = open(os.path.join(parameters["output_dir"],parameters["gid"]+".gff"), 'w')
-    if not response.ok:
-        sys.stderr.write("API not responding. Please try again later.\n")
-        sys.exit(2)
-    else:
-        for block in response.iter_content(1024):
-            handle.write(block)
+    target_file =os.path.join(parameters["output_path"],parameters["gid"]+".gff")
+    if not os.path.exists(target_file):
+        annotation_url= "data_url/genome_feature/?and(eq(genome_id,gid),eq(annotation,PATRIC),or(eq(feature_type,CDS),eq(feature_type,tRNA),eq(feature_type,rRNA)))&limit(25000)".replace("data_url",parameters["data_url"]).replace("gid",parameters["gid"])
+        print annotation_url
+        headers = {"accept":"application/cufflinks+gff"}
+        #print "switch THE HEADER BACK!"
+        #headers = {'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'}
+        req = requests.Request('GET', annotation_url, headers=headers)
+        prepared = req.prepare()
+        #pretty_print_POST(prepared)
+        s = requests.Session()
+        response=s.send(prepared)
+        handle = open(target_file, 'w')
+        if not response.ok:
+            sys.stderr.write("API not responding. Please try again later.\n")
+            sys.exit(2)
+        else:
+            for block in response.iter_content(1024):
+                handle.write(block)
+    return target_file
 
 def get_files(job_data, server_data):
-    genome_dirs=[job_data.output_dir]
-    job_data["data_url"]=server_data.data_url
-    for g in job_data.genome_ids:
+    genome_dirs=[job_data["output_path"]]
+    job_data["data_url"]=server_data["data_url"]
+    job_data["gid"]=job_data["reference_genome_id"]
+    for g in [job_data["reference_genome_id"]]:
         get_annotation(job_data)
         get_genome(job_data)
     return genome_dirs
 
-def run_alignment(genome_list, library_dict, parameters, output_dir): 
+def run_alignment(genome_list, library_dict, parameters): 
     #modifies library_dict sub replicates to include 'bowtie' dict recording output files
+    output_path=parameters["output_path"]
+    key_handle=open(os.path.join(parameters["output_path"],"output_keys.txt"), 'w')
     for genome in genome_list:
-        genome_link=os.path.join(output_dir, os.path.basename(genome["genome"]))
+        genome_link=os.path.join(output_path, os.path.basename(genome["genome"]))
         final_cleanup=[]
         if not os.path.exists(genome_link):
             subprocess.check_call(["ln","-s",genome["genome"],genome_link])
-        cmd=["tpp", "-bwa", "bwa", "-ref", genome.genome]
+        cmd=["tpp", "-bwa", "bwa", "-ref", genome["genome"]]
         #thread_count=multiprocessing.cpu_count()
         #cmd+=["-p",str(thread_count)]
         #if genome["dir"].endswith('/'):
         #    genome["dir"]=genome["dir"][:-1]
         #genome["dir"]=os.path.abspath(genome["dir"])
-        #genome["output"]=os.path.join(output_dir,os.path.basename(genome["dir"]))
+        #genome["output"]=os.path.join(output_path,os.path.basename(genome["dir"]))
         for library in library_dict:
             rcount=0
             for r in library_dict[library]["replicates"]:
                 cur_cleanup=[]
                 rcount+=1
-                target_dir=output_dir
+                result_name=library+str(rcount)
+                target_dir=output_path
             #    target_dir=os.path.join(genome["output"],library,"replicate"+str(rcount))
             #    target_dir=os.path.abspath(target_dir)
             #    subprocess.call(["mkdir","-p",target_dir])
@@ -79,17 +89,19 @@ def run_alignment(genome_list, library_dict, parameters, output_dir):
                     cur_cmd+=["-reads1",r["read1"]," -reads2",r["read2"]]
                     name1=os.path.splitext(os.path.basename(r["read1"]))[0]
                     name2=os.path.splitext(os.path.basename(r["read2"]))[0]
-                    base_name=os.path.join(target_dir,name1+"_"+name2)
+                    key_handle.write("\t".join([name1,name2,result_name])+"\n")
+                    base_name=os.path.join(target_dir,result_name)
                 else:
                     cur_cmd+=["-reads1",r["read1"]]
                     name1=os.path.splitext(os.path.basename(r["read1"]))[0]
-                    base_name=os.path.join(target_dir,name1)
-                sam_file = base_name+".sam"
+                    key_handle.write("\t".join([name1,result_name])+"\n")
+                    base_name=os.path.join(target_dir,result_name)
+                sam_file = result_name+".sam"
                 cur_cleanup.append(sam_file)
                 bam_file=sam_file[:-4]+".bam"
                 r[genome["genome"]]={}
                 r[genome["genome"]]["bam"]=bam_file
-                cur_cmd+=["-S",sam_file]
+                cur_cmd+=["-output",base_name]
                 if os.path.exists(bam_file):
                     sys.stderr.write(bam_file+" alignments file already exists. skipping\n")
                 else:
@@ -102,12 +114,13 @@ def run_alignment(genome_list, library_dict, parameters, output_dir):
                     #subprocess.check_call('samtools sort %s %s' % (bam_file+".tmp", bam_file), shell=True)
                 for garbage in cur_cleanup:
                     subprocess.call(["rm", garbage])
+        key_handle.close()
         for garbage in final_cleanup:
             subprocess.call(["rm", garbage])
 
 
 def main(server_setup, job_data):
-    required_data=["library_names","output_dir","read_files","genome_ids"]
+    required_data=["experimental_conditions","read_files","reference_genome_id", "recipe"]
     for data in required_data:
         if not data in job_data or len(job_data[data]) == 0:
             sys.stderr.write("Missing "+ data +"\n")
@@ -115,23 +128,26 @@ def main(server_setup, job_data):
     
     library_dict={}
     library_list=[]
-    library_list=job_data["library_names"]
-    output_dir=job_data.output_dir
-    for lib in library_list:
-        library_dict[lib]={"library":lib}
-    count=0
+    library_list=job_data["experimental_conditions"]
+    output_path=job_data["output_path"]=os.path.abspath(job_data["output_path"])
+    #for lib in library_list:
+    #    library_dict[lib]={"library":lib}
+    #job_data["read_files"]=job_data["read_files"].split()
+    #count=0
     #add read/replicate structure to library dict
-    for read in job_data.readfiles:
-        replicates=read.split(',')
-        rep_store=library_dict[library_list[count]]["replicates"]=[]
-        for rep in replicates:
-            pair=rep.split('%')
-            pair_dict={"read1":pair[0]}
-            if len(pair) == 2:
-                pair_dict["read2"]=pair[1]
-            rep_store.append(pair_dict)
-        count+=1
-    genome_dirs=get_files(job_data.genome_ids)
+    #for read in job_data["read_files"]:
+    #    replicates=read.split(',')
+    #    rep_store=library_dict[library_list[count]]["replicates"]=[]
+    #    for rep in replicates:
+    #        pair=rep.split('%')
+    #        pair_dict={"read1":pair[0]}
+    #        if len(pair) == 2:
+    #            pair_dict["read2"]=pair[1]
+    #        rep_store.append(pair_dict)
+    #    count+=1
+   
+    library_dict = job_data["read_files"] 
+    genome_dirs=get_files(job_data, server_setup)
     genome_list=[]
     for g in genome_dirs:
         cur_genome={"genome":[],"annotation":[],"dir":g,"hisat_index":[]}
@@ -160,21 +176,23 @@ def main(server_setup, job_data):
 
 
         genome_list.append(cur_genome)
-    output_dir=os.path.abspath(output_dir)
-    subprocess.call(["mkdir","-p",output_dir])
-    run_alignment(genome_list, library_dict, parameters, output_dir)
-    run_transit(genome_list, library_dict, parameters, output_dir)
-    cleanup(genome_list, library_dict, parameters, output_dir)
+    output_path=os.path.abspath(output_path)
+    if not os.path.exists(output_path):
+        subprocess.call(["mkdir","-p",output_path])
+    run_alignment(genome_list, library_dict, job_data)
+    #run_transit(genome_list, library_dict, parameters, output_path)
+    #cleanup(genome_list, library_dict, parameters, output_path)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     jobinfo = parser.add_mutually_exclusive_group(required=True)
     jobinfo.add_argument('--jfile', 
-            help='json file for job {"genome_ids":[x],"library_names":[x], "transit_params":{key:value}, "output_dir":x, "read_files":x')
+            help='json file for job {"reference_genome_id":[x],"experimental_conditions":[x], "transit_params":{key:value}, "output_path":x, "read_files":x')
     jobinfo.add_argument('--jstring', help='json string from user input')
     serverinfo = parser.add_mutually_exclusive_group(required=True)
     serverinfo.add_argument('--sfile', help='server setup JSON file')
     serverinfo.add_argument('--sstring', help='server setup JSON string')
+    parser.add_argument('-o', help='output directory. defaults to current directory.', required=False)
 
     #parser.add_argument('-g', help='genome ids to get *.fna and annotation *.gff', required=True)
     #parser.add_argument('-L', help='csv list of library names for comparison', required=True)
@@ -198,5 +216,5 @@ if __name__ == "__main__":
     except:
         sys.stderr.write("Failed to parse server data\n")
         raise
-    
+    job_data["output_path"]=args.o 
     main(server_setup, job_data)
